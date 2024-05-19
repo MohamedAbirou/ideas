9e7d6e40920274c328c1c8d606cbd3da9bb26224
Mohamed Abirou <abiroumohamed58@gmail.com>
2024-05-19 21:23:39 +0100

    Deployment preparation

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -2,6 +2,7 @@
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
+composer install --optimize-autoloader --no-dev
 
 echo "generating application key..."
 php artisan key:generate --show
